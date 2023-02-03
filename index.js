54a22e7f7c1d6ca2b16f58e9023f24c60a739228
markflemingblog <121588784+markflemingblog@users.noreply.github.com>
2023-02-03 12:00:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const dropdown = document.getElementById("dropdown");
-dropdown.addEventListener("change", function() {
-  const selectedValue = dropdown.value;
-  const allDivs = document.querySelectorAll("div");
-  allDivs.forEach(function(div) {
+document.getElementById("dropdown").addEventListener("change", function() {
+  const selectedValue = this.value;
+  const allDivs = document.querySelectorAll("div[id^='div']");
+
+  allDivs.forEach(div => {
     if (div.id === selectedValue) {
-      div.classList.add("selected");
       div.style.display = "block";
     } else {
-      div.classList.remove("selected");
       div.style.display = "none";
     }
   });
